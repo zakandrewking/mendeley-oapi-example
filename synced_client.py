8a3d0a68a32182c648227d1818df89b6e9861650
Nicolas Esteves <nicolas.esteves@mendeley.com>
2012-08-16 19:32:19 +0100

    WIP added an example of client syncing + tests

---
+++ synced_client.py
@@ -0,0 +1,301 @@
+from mendeley_client import *
+
+class SyncStatus:
+    Deleted = 0
+    Modified = 1
+    New = 2
+    Synced = 3   
+
+    @staticmethod
+    def to_str(status):
+        return ["DEL","MOD","NEW","SYN"][status]
+
+class Object:
+    pass
+
+class SyncedObject:
+
+    def __init__(self, obj, status=SyncStatus.New):
+        self.reset(obj, status)
+        
+    def reset(self, obj, status):
+        self.changes = {}
+        self.status = status
+
+        if isinstance(obj, dict):
+            self.object = Object()
+            for key in obj.keys():
+                setattr(self.object, key, obj[key])
+        elif isinstance(obj, SyncedObject):
+            self.object = obj.object
+        else:
+            assert False
+
+    def version(self):
+        return self.object.version
+
+    def id(self):
+        return self.object.id
+
+    def update(self, change):
+        if len(change.keys()) == 0:
+            return
+        # TODO add some checking of the keys etc
+        for key, value in change.items():
+            self.changes[key] = value
+        
+        self.status = SyncStatus.Modified
+
+    def apply_changes(self):
+        if len(self.changes) == 0:
+            return
+
+        for key, value in self.changes.items():
+            setattr(self.object, key, value)
+
+    def to_json(self):
+        obj = {}
+
+        for key in vars(self.object):
+            obj[key] = getattr(self.object,key)
+
+        return obj
+
+    def is_deleted(self):
+        return self.status == SyncStatus.Deleted
+
+    def is_modified(self):
+        return self.status == SyncStatus.Modified
+
+    def is_new(self):
+        return self.status == SyncStatus.New
+
+    def is_synced(self):
+        return self.status == SyncStatus.Synced
+
+    def delete(self):
+        self.status = SyncStatus.Deleted
+
+    #####
+    def local_delete(self):
+        self.status = SyncStatus.Deleted
+        
+    def local_update(self, change):
+        self.status = self.Modified
+        self.changes.append(obj)
+        
+        
+class SyncedFolder(SyncedObject):
+    pass
+
+class SyncedDocument(SyncedObject):
+
+    def __str__(self):
+        return self.object.id
+
+from pprint import pprint
+
+
+class ConflictResolver:
+
+    def resolve_both_updated(self, local_document, server_document):
+        raise Exception("Reimplement me")
+
+    def resolve_local_delete_before_update(self, sid, server_document):
+        raise Exception("Reimplement me")
+
+class SimpleConflictResolver(ConflictResolver):
+
+    def resolve_local_delete_before_update(self, sid, server_document):
+        keep_server_document = True
+        return keep_server_document
+
+    def resolve_both_updated(self, local_document, server_document):
+        assert isinstance(server_document, SyncedDocument)
+        assert isinstance(local_document, SyncedDocument)
+
+        server_changes = {}
+        for key in vars(server_document):
+            if getattr(server_document, key) != getattr(local_document, key):
+                server_changes[key] = getattr(server_document, key)
+
+        local_changes = local_document.changes
+        
+        for key, server_value in server_changes.items():
+            if key not in local_changes:
+                # no conflict, just accept the server changes
+                setattr(local_document, key, server_value)
+            else:
+                keep_server_version = self.resolve_conflict(key, local_changes[key], server_value)
+                if keep_server_version:
+                    setattr(local_document, key, server_value)
+                    del local_changes[key]
+                else:
+                    # the document status will stay modified and will send its
+                    # change to the server in sync_documents
+                    pass
+        
+        local_document.object.version = server_document["version"]
+
+        # if no local changes are left, the document isn't modified anymore
+        if len(local_changes) == 0:
+            local_document.status = SyncStatus.Synced
+        else:
+            # local changes will be applied in sync_documents
+            pass
+
+    def resolve_conflict(self, key, local_version, server_version):
+        # dumb "resolution", 
+        return False
+
+class DummySyncedClient:
+
+    def __init__(self, conflict_resolver=SimpleConflictResolver()):
+        self.client = create_client("config_sync.json")
+        self.folders = {}
+        self.documents = {}
+        assert isinstance(conflict_resolver, ConflictResolver)
+        self.conflict_resolver = conflict_resolver
+
+    def sync(self):
+        success = False
+        
+        while True:
+            # if not self.sync_folders():
+            #     continue
+            if not self.sync_documents():
+                continue
+            break
+
+    def fetch_document(self, sid):
+        details = self.client.document_details(sid)
+        assert "error" not in details
+        assert details["id"] == sid  
+        return SyncedDocument(details, SyncStatus.Synced)
+
+    def sync_documents(self):
+        # TODO fetch the whole library with paging
+        # handle new docs in self.documents
+        # validate folders before storing, restart sync if unknown folder
+
+        server_documents = self.client.library()
+        assert "error" not in server_documents
+        server_ids = []
+        for server_document in server_documents["documents"]:
+            sid = server_document["id"]
+            sdoc = SyncedDocument(server_document, SyncStatus.Synced)
+            server_ids.append(sid)
+            if sid not in self.documents:
+                # new document
+                self.documents[sid] = self.fetch_document(sid)
+                assert self.documents[sid].object.id == sid
+                continue
+            
+            local_document = self.documents[sid]
+            assert not local_document.is_new()
+
+            # if server version is more recent
+            if local_document.version() != sdoc.version():
+                sdoc = self.fetch_document(sid)
+                if local_document.is_deleted():
+                    keep_server = self.conflict_resolver.resolve_local_delete_remote_update(local_document, sdoc)
+                    if keep_server:
+                        self.documents[sid].reset(sdoc, SyncStatus.Synced)
+                    else:
+                        # will be deleted later
+                        pass
+                    continue
+
+                if local_document.is_synced():
+                    # update from server
+                    local_document.reset(sdoc, SyncStatus.Synced)
+                    continue
+
+                if local_document.is_modified():
+                    # both documents are modified, resolve the conflict
+                    # by handling the server changes required and leave the local 
+                    # changes to be synced later
+                    self.conflict_resolver.resolve_both_updated(local_document, sdoc)
+                    assert local_document.version() == sdoc.version()
+                    continue
+
+                # all cases should have been handled
+                assert False
+
+            # both have the same version, so only local changes possible
+            else:
+                if local_document.is_synced():
+                    # nothing to do
+                    # assert sdoc == local_document
+                    continue
+
+                if local_document.is_deleted():
+                    # nothing to do, will be deleted
+                    continue
+
+                if local_document.is_modified():
+                    # nothing to do, changes will be sent in the update loop
+                    continue
+                
+                # all cases should have been handled
+                assert False
+
+        for doc_id in self.documents.keys():
+            local_document = self.documents[doc_id]
+            assert local_document.object.id == doc_id
+
+            if doc_id not in server_ids:
+                if not local_document.is_new():
+                    # was deleted on the server
+                    del self.documents[doc_id]
+                    continue
+                else:
+                    response = self.client.create_document(document=local_document.to_json())
+                    assert "error" not in response
+
+                    local_document.object.version = response["version"]
+                    local_document.object.id = response["document_id"]
+                    local_document.status = SyncStatus.Synced
+                    continue   
+
+            if local_document.is_synced():
+                continue                 
+
+            # shouldn't happen, if it's new the server shouldn't know about it
+            assert not local_document.is_new()
+            assert not local_document.is_synced()
+
+            if local_document.is_deleted():
+                response = self.client.delete_library_document(doc_id)
+                assert "error" not in response
+                del self.documents[doc_id]
+                continue
+
+            if local_document.is_modified():
+                response = self.client.update_document(doc_id, document=local_document.changes)
+                assert "error" not in response
+                local_document.status = SyncStatus.Synced
+                local_document.object.version = response["version"]
+                local_document.apply_changes()
+                continue
+
+            assert False
+
+        return True
+
+    def reset(self):
+        self.documents = {}
+        self.folders = {}
+
+    def dump_status(self,outf):
+        outf.write( "\n")
+        outf.write( "#Documents (%d)\n"%len(self.documents))
+        outf.write( "@sort 0\n")
+        outf.write( "@group 0\n")
+        outf.write( "--\n")
+        outf.write( "status, id, version, title\n")
+        if len(self.documents):
+
+            for sid, document in self.documents.items():
+                outf.write( "%s,  %s ,  %s , %s\n"%(SyncStatus.to_str(document.status), document.object.id, document.version(), document.object.title))
+        
---
+++ test-sync.py
@@ -0,0 +1,214 @@
+from pprint import pprint
+from synced_client import *
+import os
+import unittest
+import sys
+import time
+import datetime
+import calendar
+
+def skip(fn):
+
+    def wrapped(*args, **kwargs):
+        print "Skipping %s"%fn.__name__
+        return
+
+    return wrapped
+
+debug = True 
+
+class TestDocumentsSyncing(unittest.TestCase):
+
+    def log(self, message):
+        if debug:
+            self.log_file.write("\n#%s\n"%message)
+            self.log_file.write("#"+"-"*len(message)+"\n\n")
+            self.sclient.dump_status(self.log_file)
+
+    def clear_library(self):
+        for doc in self.sclient.client.library()["document_ids"]:
+            self.sclient.client.delete_library_document(doc)
+
+    def seed_library(self, count):
+        ids = []
+        for i in range(count):
+            response = self.sclient.client.create_document(document={"type":"Book", "title":"Title %d"%i})
+            ids.append(response["document_id"])
+        self.assertEqual(len(ids), count)
+        return ids
+
+    def document_exists(self, document_id):
+        return "error" not in self.sclient.client.document_details(document_id)
+      
+    def setUp(self):
+        self.sclient = DummySyncedClient()
+        self.clear_library()
+        self.log_file = open("/tmp/log.txt","w")
+        
+    def tearDown(self):
+        pass
+
+    @skip
+    def test_fetch(self):
+        count = 5
+        ids = self.seed_library(count)
+        
+        # sync, should have count documents with matching ids
+        self.sclient.sync()
+        self.assertEqual(len(self.sclient.documents), count)
+        self.assertEqual(sorted(ids), sorted(self.sclient.documents.keys()))
+
+        # the status of all documents should be synced
+        for document in self.sclient.documents.values():
+            self.assertTrue(document.is_synced())
+
+    @skip
+    def test_local_delete(self):
+        count = 5
+        ids = self.seed_library(count)
+        self.sclient.sync()
+
+        # locally delete 1 document
+        deletion_id = ids[count/2]
+        local_document = self.sclient.documents[deletion_id]
+        local_document.delete()
+        # the status of the document should be deleted, the count
+        # should stay the same until synced
+        self.assertTrue(local_document.is_deleted())
+        self.assertEqual(len(self.sclient.documents), count)
+        
+        # check that the status of the documents are correct
+        for docid, document in self.sclient.documents.items():
+            if docid == deletion_id:
+                self.assertTrue(document.is_deleted())
+            else:
+                self.assertTrue(document.is_synced())
+        self.log("After local delete")
+
+        # sync the deletion
+        self.sclient.sync()
+        self.log("After sync")
+        
+        # make sure the document doesn't exist anymore 
+        self.assertEqual(len(self.sclient.documents), count-1)
+        self.assertTrue(deletion_id not in self.sclient.documents.keys())
+
+        # make sure the other documents are unaffected
+        for document in self.sclient.documents.values():
+            self.assertTrue(document.is_synced())
+            self.assertTrue(document.id() in ids)
+            self.assertTrue(document.id() != deletion_id)
+        
+        # check on the server that the deletion was done
+        for doc_id in ids:
+            if doc_id == deletion_id:
+                self.assertFalse(self.document_exists(doc_id))
+            else:
+                self.assertTrue(self.document_exists(doc_id))
+        
+    @skip
+    def test_server_delete(self):
+        count = 5 
+        ids = self.seed_library(count)
+        self.sclient.sync()
+
+        # delete one doc on the server
+        self.sclient.client.delete_library_document(ids[0])
+        self.assertFalse(self.document_exists(ids[0]))
+
+        self.sclient.sync()
+        self.log("After sync")
+        self.assertEqual(len(self.sclient.documents), count-1)
+        self.assertTrue(ids[0] not in self.sclient.documents.keys())
+
+        for doc_id in ids[1:]:
+            self.assertTrue(doc_id in self.sclient.documents.keys())
+            self.assertTrue(self.sclient.documents[doc_id].is_synced())
+        
+
+    # @skip            
+    # def test_nop(self):
+    #     pass
+
+    # @skip            
+    # def test_local_update_remote_delete(self):
+    #     pass
+
+    # @skip            
+    # def test_local_update_remote_update_conflict(self):
+    #     pass
+
+    # @skip            
+    def test_local_update_remote_update_no_conflict(self):
+        pass
+
+    @skip            
+    def test_local_update(self):
+        new_title = "updated_title"
+
+        count = 5 
+        ids = self.seed_library(count)
+        self.sclient.sync()     
+
+        # change the title of one document
+        local_document = self.sclient.documents[ids[0]]
+        local_document.update({"title":new_title})
+
+        original_version = local_document.version()
+        
+        # the document should be marked as modified
+        self.assertTrue(local_document.is_modified())
+        for doc_id in ids[1:]:
+            self.assertTrue(self.sclient.documents[doc_id].is_synced())
+        
+        self.log("Before sync")
+        self.sclient.sync()
+        self.log("After sync")
+
+        # all documents should be synced now
+        for doc_id in ids:
+            self.assertTrue(self.sclient.documents[doc_id].is_synced())
+            self.assertTrue(self.document_exists(doc_id))
+
+        self.assertEqual(local_document.object.title, new_title)
+        self.assertTrue(local_document.version() > original_version)
+        
+        details = self.sclient.client.document_details(ids[0])
+        self.assertEqual(details["title"], new_title)
+        self.assertEqual(details["version"], local_document.version())
+        
+
+    @skip            
+    def test_remote_update(self):
+        new_title = "updated_title"
+
+        count = 5 
+        ids = self.seed_library(count)
+        self.sclient.sync()        
+        
+        local_document = self.sclient.documents[ids[0]]
+        original_version = self.sclient.documents[ids[0]].version()
+
+        # update the title of a document on the server
+        response = self.sclient.client.update_document(ids[0], document={"title":new_title})
+        self.assertTrue("error" not in response)
+
+        # make sure the title was updated on the server
+        details = self.sclient.client.document_details(ids[0])
+        self.assertEqual(details["title"], new_title)  
+      
+        self.sclient.sync()
+
+        # all documents should be synced
+        for doc_id in ids:
+            self.assertTrue(self.sclient.documents[doc_id].is_synced())
+            self.assertTrue(self.document_exists(doc_id))       
+
+        self.assertEqual(local_document.object.title, new_title)
+        self.assertTrue(local_document.version() > original_version)            
+        
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
